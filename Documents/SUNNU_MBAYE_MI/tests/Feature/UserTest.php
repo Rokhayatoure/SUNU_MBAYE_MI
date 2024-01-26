e564d3265912a41340cf6079ccd6bbc87f36a316
Rokhaya Toure <rokhayatoure2000@gmail.com>
2024-01-26 09:25:48 -0800

    usyig

---
+++ Documents/SUNNU_MBAYE_MI/tests/Feature/UserTest.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class UserTest extends TestCase
+{
+    Use RefreshDatabase;
+
+    // public function (): void
+    // {
+    //     $response = $this->get('/');
+
+    //     $response->assertStatus(200);
+    // }
+}
